4bc60aa9fa0224c88bd933a335f66ae30d144482
AerGameChannel <63594377+AerGameChannel@users.noreply.github.com>
2020-05-05 12:30:51 +0300

    Add files via upload

---
+++ BreakDoors-WWS/EventHandler.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using WW_SYSTEM;
+using WW_SYSTEM.API;
+using WW_SYSTEM.EventHandlers;
+using WW_SYSTEM.Events;
+
+namespace BreakDoors
+{
+    public class EventHandler: IEventHandlerAdminQuery
+    {
+        public Plugin plugin;
+        public Plugin BreakDoors;
+
+        public EventHandler(BreakDoors plugin)
+        {
+            this.plugin = plugin;
+        }
+        public void OnAdminQuery(AdminQueryEvent ev)
+        {
+            string[] array = ev.Query.Split(new char[]
+            {
+                    ' '
+            });
+            if (ev.Query.ToLower().StartsWith("bd"))
+            {
+                if (array.Length <= 1)
+                {
+                    ev.Output = "BreakDoors#Usage: bd <RemoteAdmin player id>";
+                    ev.Successful = true;
+                    ev.Handled = true;
+                    return;
+                }
+                if (string.IsNullOrEmpty(array[1]))
+                {
+                    ev.Output = "BreakDoors#Usage: bd <RemoteAdmin player id>";
+                    ev.Successful = true;
+                    ev.Handled = true;
+                    return;
+                }
+                if (array.Length > 1)
+                {
+                    try
+                    {
+                        if (!ev.Admin.IsPermitted(PlayerPermissions.PlayersManagement))
+                        {
+                            ev.Output = "BreakDoors#Not enough permissions";
+                            ev.Successful = false;
+                            ev.Handled = true;
+                            return;
+                        }
+                        if (array.Length > 0)
+                        {
+                            if (array[1].ToLower().Contains("help"))
+                            {
+                                ev.Output = "BreakDoors#Usage: bd <RemoteAdmin player id>";
+                                ev.Successful = true;
+                                ev.Handled = true;
+                                return;
+                            }
+                            else
+                            {
+                                    int id = int.Parse(array[1]);
+
+                                Player pl = Server.Round.FindPlayerWithId(id);
+                                if (pl != null && !pl.BreakDoors)
+                                {
+                                    pl.BreakDoors = true;
+
+                                    ev.Output = "BreakDoors#Enabled BreakDoors for player " + pl.Nick;
+                                    ev.Successful = true;
+                                    ev.Handled = true;
+                                    return;
+                                }
+                                else if (pl != null && pl.BreakDoors)
+                                {
+                                    pl.BreakDoors = false;
+
+                                    ev.Output = "BreakDoors#Disabled BreakDoors for player " + pl.Nick;
+                                    ev.Successful = true;
+                                    ev.Handled = true;
+                                    return;
+                                }
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        ev.Output = "BreakDoors#Error: " + ex;
+                        ev.Successful = false;
+                        ev.Handled = true;
+                        return;
+                    }
+                }
+            }
+        }
+    }
+}  
