846c667908d64b8adbe022d21519b7bfd86de8cf
AerGameChannel <63594377+AerGameChannel@users.noreply.github.com>
2020-05-05 12:33:17 +0300

    Add files via upload

---
+++ BreakDoors-WWS/Plugin.cs
@@ -0,0 +1,36 @@
+﻿using WW_SYSTEM;
+using WW_SYSTEM.Attributes;
+
+namespace BreakDoors
+{
+    [PluginDetails(
+        author = "Aer",
+        name = "BreakDoors",
+        description = "BreakDoors",
+        id = "breakdoors.plugin",
+        configPrefix = "BreakDoors",
+        version = "1.0",
+        WWSYSTEMMajor = 6,
+        WWSYSTEMMinor = 3,
+        WWSYSTEMRevision = 1
+        )]
+    public class BreakDoors : Plugin
+    {
+        public override void OnDisable()
+        {
+            this.Info(this.Details.name + " was disabled");
+        }
+
+        public override void OnEnable()
+        {
+            bool enabled = this.Config.GetBool("breakdoors_enable", true);
+            if (!enabled) return;
+            this.Info(this.Details.name + " has loaded");
+        }
+
+        public override void Register()
+        {
+            AddEventHandlers(new EventHandler(this));
+        }
+    }
+}
